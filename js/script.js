eaea30c6c825f2d90b0435fd247e740a00a2a959
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2023-11-21 10:32:04 +0100

    Exercise 2.2

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
-// **Esercizio 1**
+// *****Esercizio 1*****
 let number = 100;
 console.log(number);
 
-// **Esercizio 2**
+// *****Esercizio 2*****
 const pi = 3.14;
 console.log(pi);
 
-// **Esercizio 2/B**
+// *****Esercizio 2/B*****
 // pi = 3.1416;
 // console.log(pi)
 
-// **Esercizio 2/C**
+// *****Esercizio 2/C*****
 let radius = 8;
 
 let circle = radius * 2 * pi
 console.log(circle)
 
-// **Esercizio 3**
+// *****Esercizio 3*****
 let name = 'Carlo';
 name = 'Marco';
 console.log(name)
 
-// **Esercizio 4**
+// *****Esercizio 4*****
 let greet = 'Ciao, Mondo!';
 console.log(greet);
 
-// **Esercizio 4/B**
+// *****Esercizio 4/B*****
 // let greet = 'come stai mondo?'
 // console.log(greet)
 
-// **Esercizio 4/C**
+// *****Esercizio 4/C*****
 // greet = greet + ' Come stai?'
 // console.log(greet)
 
@@ -37,19 +37,36 @@ greet = `${greet} Come stai?`
 console.log(greet)
 
 // IF-ELSE
-// **Esercizio 1**
+
+// *****Esercizio 1.2*****
 let num1 = 40;
 
 let num2 = 20;
 
+// let comparison;
+
+// if(num1 > num2){
+//    comparison = num1
+// }
+
+// else{
+//     comparison = num2
+// }
+
+// console.log(comparison)
+
+// *****Esercizio 2.2*****
+let stringa1 = 'ciao';
+
+let stringa2 = 'come stai';
+
 let comparison;
 
-if(num1 > num2){
-   comparison = num1
+if(stringa1 == stringa2){
+    comparison = 'Uguale';
 }
-
 else{
-    comparison = num2
+    comparison = 'Diverso';
 }
 
-console.log(comparison)
+console.log(comparison);
