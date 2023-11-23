55baf58b7a3662b25695a866890cf57d88732fb0
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2023-11-23 12:55:47 +0100

    Exercise 2/array

--- js/script.js
+++ js/script.js
@@ -100,5 +100,10 @@ console.log(compara);
 // ARRAY
 
 // *****Esercizio 1/array***** 
-const numbers= [1,2,3,4,5,6,7,8];
+const numbers = [1,2,3,4,5,6,7,8];
 console.log(numbers);
+
+// *****Esercizio 2/array***** 
+const fruits = ['mela', 'banana', 'pesca', 'pera', 'albicocca', 
+'mango', 'kiwi', 'papaya'];
+console.log(fruits);
